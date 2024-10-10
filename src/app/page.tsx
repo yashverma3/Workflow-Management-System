b7f853bb0ef65c0fb73b42400f29f86f3b92013b
Yash Verma <yeshverma5122@gmail.com>
2024-10-10 19:50:08 +0530

    Customize components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click Me</Button>
-      <p className="text-red-500 font-semibold">
-        Yash
-      </p>
+      <Input />
+      <Button variant="primary">
+        Primary
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="destructive">
+        Destructive
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
+      <Button variant="muted">
+        Muted
+      </Button>
+      <Button variant="outline">
+        Outine
+      </Button>
+      <Button variant="teritary">
+        Teritary
+      </Button>
     </div>
   );
 }
