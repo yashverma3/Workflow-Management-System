a2aa71031b9767f29ac4553386519ed2e587ba98
Yash Verma <yeshverma5122@gmail.com>
2024-10-16 01:28:06 +0530

    Create a workspace switcher

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -8,6 +8,16 @@ import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 import { createWorkspaceSchema } from "../schemas";
 
 const app = new Hono()
+    .get("/", sessionMiddleware, async (c) => {
+        const databases = c.get("databases");
+
+        const workspaces = await databases.listDocuments(
+            DATABASE_ID,
+            WORKSPACES_ID,
+        );
+
+        return c.json({ data: workspaces });
+    })
     .post(
         "/",
         zValidator("form", createWorkspaceSchema),
