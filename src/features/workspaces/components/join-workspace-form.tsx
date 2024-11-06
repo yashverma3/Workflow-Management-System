5665215150ac032596f622769f0039fc55ee848c
Yash Verma <yeshverma5122@gmail.com>
2024-11-07 02:36:11 +0530

    Build the invite system

---
+++ src/features/workspaces/components/join-workspace-form.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card";
+
+import { useJoinWorkspace } from "../api/use-join-workspace";
+import { useInviteCode } from "../hooks/use-invite-code";
+import { useWorkspaceId } from "../hooks/use-workspace-id";
+
+interface JoinWorkspaceFormProps {
+    initialValues: {
+        name: string;
+    };
+};
+
+export const JoinWorkspaceForm = ({
+    initialValues,
+}: JoinWorkspaceFormProps) => {
+    const router = useRouter();
+    const workspaceId = useWorkspaceId();
+    const inviteCode = useInviteCode();
+    const { mutate, isPending } = useJoinWorkspace();
+
+    const onSubmit = () => {
+        mutate({
+            param: { workspaceId },
+            json: { code: inviteCode }
+        }, {
+            onSuccess: ({ data }) => {
+                router.push(`/workspaces/${data.$id}`);
+            },
+        });
+    };
+
+    return (
+        <Card className="w-full h-full border-none shadow-none">
+            <CardHeader className="p-7">
+                <CardTitle className="text-xl font-bold">
+                    Join workspace
+                </CardTitle>
+                <CardDescription>
+                    You&apos;ve been invited to join <strong>{initialValues.name}</strong> workspace
+                </CardDescription>
+            </CardHeader>
+            <div className="px-7">
+                <DottedSeparator />
+            </div>
+            <CardContent className="p-7">
+                <div className="flex flex-col lg:flex-row gap-2 items-center justify-between">
+                    <Button
+                        variant="secondary"
+                        type="button"
+                        asChild
+                        size="lg"
+                        className="w-full lg:w-fit"
+                        disabled={isPending}
+                    >
+                        <Link href="/">
+                            Cancel
+                        </Link>
+                    </Button>
+                    <Button
+                        size="lg"
+                        className="w-full lg:w-fit"
+                        type="button"
+                        onClick={onSubmit}
+                        disabled={isPending}
+                    >
+                        Join Workspace
+                    </Button>
+                </div>
+            </CardContent>
+        </Card>
+    );
+}
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { Hono } from "hono";
 import { ID, Query } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
@@ -10,6 +11,7 @@ import { sessionMiddleware } from "@/lib/session-middleware";
 import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
 
 import { createWorkspaceSchema, updateWorkspaceSchema } from "../schemas";
+import { Workspace } from "../types";
 
 const app = new Hono()
     .get("/", sessionMiddleware, async (c) => {
@@ -202,6 +204,51 @@ const app = new Hono()
                 workspaceId,
                 {
                     inviteCode: generateInviteCode(6),
+                },
+            );
+
+            return c.json({ data: workspace });
+        }
+    )
+    .post(
+        "/:workspaceId/join",
+        sessionMiddleware,
+        zValidator("json", z.object({ code: z.string() })),
+        async (c) => {
+            const { workspaceId } = c.req.param();
+            const { code } = c.req.valid("json");
+
+            const databases = c.get("databases");
+            const user = c.get("user");
+
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id,
+            });
+
+            if (member) {
+                return c.json({ error: "Already a member" }, 400);
+            }
+
+            const workspace = await databases.getDocument<Workspace>(
+                DATABASE_ID,
+                WORKSPACES_ID,
+                workspaceId
+            );
+
+            if (workspace.inviteCode !== code) {
+                return c.json({ error: "Invalid invite code" }, 400);
+            }
+
+            await databases.createDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                ID.unique(),
+                {
+                    workspaceId,
+                    userId: user.$id,
+                    role: MemberRole.MEMBER,
                 },
             );
 
