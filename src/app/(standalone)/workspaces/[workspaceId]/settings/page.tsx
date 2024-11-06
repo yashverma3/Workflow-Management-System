27b13d437754288ccf9cff93a72ba719fe8c8541
Yash Verma <yeshverma5122@gmail.com>
2024-11-06 23:05:57 +0530

    Refactor server queries

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
-import { getCurrent } from "@/features/auth/actions";
-import { getWorkspace } from "@/features/workspaces/actions";
+import { getCurrent } from "@/features/auth/queries";
+import { getWorkspace } from "@/features/workspaces/queries";
 import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
 
 interface WorkspaceIdSettingsPageProps {
