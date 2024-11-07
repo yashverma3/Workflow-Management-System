780ecbaf6d8ef0513e7fd160d394930c3ec72333
Yash Verma <yeshverma5122@gmail.com>
2024-11-07 23:17:37 +0530

    Enable member update and deletion functionality

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -44,5 +44,8 @@ export async function createAdminClient() {
         get account() {
             return new Account(client);
         },
+        get users() {
+            return new Users(client);
+        }
     };
 };
