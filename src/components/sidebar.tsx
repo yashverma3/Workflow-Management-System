1bf194db52e99ccde2609c42efd9f2eb8faec60d
Yash Verma <yeshverma5122@gmail.com>
2024-11-25 20:01:58 +0530

    Add workspace projects

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { Projects } from "./ui/projects";
 import { Navigation } from "./navigation";
 import { DottedSeparator } from "./dotted-separator";
 import { WorkspaceSwitcher } from "./workspace-switcher";
@@ -15,6 +16,8 @@ export const Sidebar = () => {
             <WorkspaceSwitcher />
             <DottedSeparator className="my-4" />
             <Navigation />
+            <DottedSeparator className="my-4" />
+            <Projects />
         </aside>
     );
 };
